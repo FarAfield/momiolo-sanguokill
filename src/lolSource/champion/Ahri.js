408d8f2aac2e85906e53065d03a7af6cec95ff99
陈武 <0380010016@hxct.com>
2024-11-21 10:59:42 +0800

    更新目录

--- src/lolSource/champion/Ahri.js
+++ src/lolSource/champion/Ahri.js
@@ -6,130 +6,71 @@ function importModule() {
     name: "阿狸",
     avatar: "Ahri.png",
     skins: [
-      {
-        name: "default",
-        skin: "Ahri_0.jpg",
-      },
-      {
-        name: "高丽风情 阿狸",
-        skin: "Ahri_1.jpg",
-      },
-      {
-        name: "暗影妖狐 阿狸",
-        skin: "Ahri_2.jpg",
-      },
-      {
-        name: "焰尾妖狐 阿狸",
-        skin: "Ahri_3.jpg",
-      },
-      {
-        name: "偶像歌手 阿狸",
-        skin: "Ahri_4.jpg",
-      },
-      {
-        name: "勇者 阿狸",
-        skin: "Ahri_5.jpg",
-      },
-      {
-        name: "风纪委员 阿狸",
-        skin: "Ahri_6.jpg",
-      },
-      {
-        name: "电玩女神 阿狸",
-        skin: "Ahri_7.jpg",
-      },
-      {
-        name: "星之守护者 阿狸",
-        skin: "Ahri_14.jpg",
-      },
-      {
-        name: "K/DA 阿狸",
-        skin: "Ahri_15.jpg",
-      },
-      {
-        name: "至臻 K/DA 阿狸",
-        skin: "Ahri_16.jpg",
-      },
-      {
-        name: "永恒之森 阿狸",
-        skin: "Ahri_17.jpg",
-      },
-      {
-        name: "灵魂莲华 阿狸",
-        skin: "Ahri_27.jpg",
-      },
-      {
-        name: "K/DA ALL OUT 阿狸",
-        skin: "Ahri_28.jpg",
-      },
-      {
-        name: "魔女 阿狸",
-        skin: "Ahri_42.jpg",
-      },
-      {
-        name: "至臻 K/DA 阿狸 (2022)",
-        skin: "Ahri_65.jpg",
-      },
-      {
-        name: "魅惑之月 阿狸",
-        skin: "Ahri_66.jpg",
-      },
-      {
-        name: "净雪之月 阿狸",
-        skin: "Ahri_76.jpg",
-      },
-      {
-        name: "殿堂传奇 阿狸",
-        skin: "Ahri_85.jpg",
-      },
-      {
-        name: "联盟不朽 阿狸",
-        skin: "Ahri_86.jpg",
-      },
+      { name: "default", skin: "Ahri_0.jpg" },
+      { name: "高丽风情 阿狸", skin: "Ahri_1.jpg" },
+      { name: "暗影妖狐 阿狸", skin: "Ahri_2.jpg" },
+      { name: "焰尾妖狐 阿狸", skin: "Ahri_3.jpg" },
+      { name: "偶像歌手 阿狸", skin: "Ahri_4.jpg" },
+      { name: "勇者 阿狸", skin: "Ahri_5.jpg" },
+      { name: "风纪委员 阿狸", skin: "Ahri_6.jpg" },
+      { name: "电玩女神 阿狸", skin: "Ahri_7.jpg" },
+      { name: "星之守护者 阿狸", skin: "Ahri_14.jpg" },
+      { name: "K/DA 阿狸", skin: "Ahri_15.jpg" },
+      { name: "至臻 K/DA 阿狸", skin: "Ahri_16.jpg" },
+      { name: "永恒之森 阿狸", skin: "Ahri_17.jpg" },
+      { name: "灵魂莲华 阿狸", skin: "Ahri_27.jpg" },
+      { name: "K/DA ALL OUT 阿狸", skin: "Ahri_28.jpg" },
+      { name: "魔女 阿狸", skin: "Ahri_42.jpg" },
+      { name: "至臻 K/DA 阿狸 (2022)", skin: "Ahri_65.jpg" },
+      { name: "魅惑之月 阿狸", skin: "Ahri_66.jpg" },
+      { name: "净雪之月 阿狸", skin: "Ahri_76.jpg" },
+      { name: "殿堂传奇 阿狸", skin: "Ahri_85.jpg" },
+      { name: "联盟不朽 阿狸", skin: "Ahri_86.jpg" },
     ],
     spells: [
       {
         id: "AhriP",
         name: "摄魂夺魄",
         icon: "Ahri_SoulEater2.png",
-        description: "在造成3点伤害后，阿狸会治疗自身。回复1点血量。",
+        description:
+          "在击杀9个小兵或野怪后，阿狸会治疗自身。<br>在参与击杀一名敌方英雄后，阿狸会以一个更高的治疗效果来治疗自身。",
       },
       {
         id: "AhriQ",
         name: "欺诈宝珠",
         icon: "AhriQ.png",
         description:
-          "阿狸投出然后收回她的宝珠，在放出时会造成1点伤害，在收回时则会造成2点真实伤害",
+          "阿狸投出然后收回她的宝珠，在放出时会沿途对敌人造成<magicDamage>{{ totaldamage }}魔法伤害</magicDamage>，在收回时则会沿途对敌人造成<trueDamage>{{ totaldamage }}真实伤害</trueDamage>。{{ spellmodifierdescriptionappend }}",
       },
       {
         id: "AhriW",
         name: "妖异狐火",
         icon: "AhriW.png",
-        description: "阿狸释放出3团狐火来追踪敌人并累计造成3点伤害",
+        description:
+          "阿狸释放出3团狐火来追踪附近的敌人们并造成<magicDamage>{{ singlefiredamage }}魔法伤害</magicDamage>，第一团之后的狐火降低至<magicDamage>{{ multifiredamage }}伤害</magicDamage>。她还会获得在{{ movementspeedduration }}秒里持续衰减的<speed>{{ movementspeed*100 }}%移动速度</speed>。{{ spellmodifierdescriptionappend }}",
       },
       {
         id: "AhriE",
         name: "魅惑妖术",
         icon: "AhriE.png",
         description:
-          "阿狸献出红唇热吻，魅惑敌人并造成1点伤害（魅惑：无法被闪避）",
+          "阿狸献出红唇热吻，<status>魅惑</status>命中的首个敌人{{ charmduration }}秒并造成<magicDamage>{{ totaldamage }}魔法伤害</magicDamage>。{{ spellmodifierdescriptionappend }}",
       },
       {
         id: "AhriR",
         name: "灵魄突袭",
         icon: "AhriR.png",
         description:
-          "阿狸灵活地突进，并对敌人发射3颗灵魄弹。这些灵魄弹每颗造成1点伤害，可再次施放至多2次",
+          "阿狸灵活地突进，并对附近的敌人们发射{{ rmaxtargetspercast }}颗灵魄弹，优先选择英雄。这些灵魄弹每颗造成<magicDamage>{{ rcalculateddamage }}魔法伤害</magicDamage>。<spellName>灵魄突袭</spellName>可以在{{ rrecastwindow }}秒内<recast>再次施放</recast>至多2次。<br /><br />在此期间，用<spellName>摄魂夺魄</spellName>吞噬一名英雄的魂魄时，会使再次施放的窗口期至多延长至{{ pdurationextension }}秒并为<spellName>灵魄突袭</spellName>提供一次额外的再次施放(至多可储存至{{ rmaxcasts }}次)。{{ spellmodifierdescriptionappend }}",
       },
     ],
     effects: {
-      AhriP: (event, game, get, ui) => {},
-      AhriQ: (event, game, get, ui) => {},
-      AhriW: (event, game, get, ui) => {},
-      AhriE: (event, game, get, ui) => {},
-      AhriR: (event, game, get, ui) => {},
+      AhriP: null,
+      AhriQ: null,
+      AhriW: null,
+      AhriE: null,
+      AhriR: null,
     },
   };
 }
-
 export { importModule };
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -13,8 +13,8 @@ import { testMain } from "@/core/zTest";
 
 const modules = import.meta.glob("@/assets/zh_CN/champion/*.json");
 async function parse() {
-  // return;
   const championResult = [];
+  const list = [];
   for (const path in modules) {
     const name = path.split("/").at(-1).replace(".json", "");
     const json = await new Promise((resolve) => {
@@ -22,12 +22,19 @@ async function parse() {
         resolve(module.data);
       });
     });
-    const result = parseData(name, json[name]);
+
+    const result = parseData(name, json[name], list);
     championResult.push(result);
   }
-  console.log(championResult[1]);
+  console.log(championResult[1], list);
 }
-function parseData(name, data) {
+function parseData(name, data, list) {
+  if (data.passive.image.full !== `${name}P.png`) {
+    list.push({
+      source: data.passive.image.full,
+      target: `${name}P.png`,
+    });
+  }
   return {
     [name]: {
       type: "champion",
@@ -45,29 +52,87 @@ function parseData(name, data) {
         {
           id: `${name}P`,
           name: data.passive.name,
-          icon: data.passive.image.full,
+          icon: `${name}P.png`,
           description: data.passive.description,
         },
       ].concat(
-        data.spells.map((i) => {
+        data.spells.map((i, index) => {
+          const array = ["Q", "W", "E", "R"];
+          if (i.id !== `${name}${array[index]}`) {
+            i.id = `${name}${array[index]}`;
+          }
+          if (i.image.full !== `${i.id}.png`) {
+            list.push({
+              source: i.image.full,
+              target: `${i.id}.png`,
+            });
+          }
           return {
             id: i.id,
             name: i.name,
-            icon: i.image.full,
+            icon: `${i.id}.png`,
             description: i.tooltip,
           };
         })
       ),
       effects: {
-        [`${name}P`]: "(event, game, get, ui) => {}",
-        [`${name}Q`]: "(event, game, get, ui) => {}",
-        [`${name}W`]: "(event, game, get, ui) => {}",
-        [`${name}E`]: "(event, game, get, ui) => {}",
-        [`${name}R`]: "(event, game, get, ui) => {}",
+        [`${name}P`]: null,
+        [`${name}Q`]: null,
+        [`${name}W`]: null,
+        [`${name}E`]: null,
+        [`${name}R`]: null,
       },
     },
   };
 }
+
+// for (let i = 0; i < list.length; i++) {
+//   const item = list[i];
+//   const fileName = Object.keys(item)[0];
+//   const filePath = path.join(directory, `${fileName}.js`);
+//   const content = `function importModule() {
+//   return ${JSON.stringify(item[fileName])} } export { importModule }
+// `;
+//   fs.writeFile(filePath, content, (err) => {
+//     if (err) {
+//       console.error(`写入文件 ${fileName} 时发生错误：`, err);
+//       return;
+//     }
+//     console.log(`文件 ${fileName} 写入成功！`);
+//   });
+// }
+
+/**  copy   */
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const sourceDir = "source";
+// const targetDir = "target";
+
+// const list = [{ source: "A_P.txt", target: "AP.txt" }];
+
+// fs.readdir(sourceDir, (err, files) => {
+//   if (err) {
+//     console.error("读取目录失败：", err);
+//     return;
+//   }
+//   files.forEach((file) => {
+//     list.forEach((i) => {
+//       if (file === i.source) {
+//         const sourcePath = path.join(sourceDir, file);
+//         const targetPath = path.join(targetDir, i.target);
+//         fs.copyFile(sourcePath, targetPath, (err) => {
+//           if (err) {
+//             console.error("复制文件失败：", err);
+//             return;
+//           }
+//           console.log(`文件 ${file} 复制成功！`);
+//         });
+//       }
+//     });
+//   });
+// });
 </script>
 <style lang="scss" scoped>
 .root {
